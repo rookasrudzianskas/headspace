b08397497f3d948a4a52e4fcd680ffffc401b01a
rokas <rookas.rudzenskas@gmail.com>
2024-11-06 11:37:17 +0200

    fix

--- app/index.tsx
+++
@@ -1,4 +0,0 @@
-import { Text } from 'react-native';
-export default function HomeScreen() {
-  return <Text>Home screen</Text>;
-}
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+export default function HomeScreen() {
+  return (
+    <Text className="font-bold text-4xl p-10 bg-red-500 color-blue-500">
+      Home screen
+    </Text>
+  );;
+}
