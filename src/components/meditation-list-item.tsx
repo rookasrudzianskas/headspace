bf4c37a5123e5d3cd4e8dd4673b81c4dffb67081
rokas <rookas.rudzenskas@gmail.com>
2024-11-07 11:02:24 +0200

    fix

--- src/components/meditation-list-item.tsx
+++ src/components/meditation-list-item.tsx
@@ -5,12 +5,37 @@ import {Meditation} from "@/types";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import {useRouter} from "expo-router";
+import { Link, router } from 'expo-router';
+import Paywall, { PAYWALL_RESULT } from 'react-native-purchases-ui';
 
 const MeditationListItem = ({meditation}: {meditation: Meditation}) => {
   const router = useRouter();
 
+  const onPress = async () => {
+    if (!meditation.pro) {
+      router.push(`/meditation/${meditation.id}`);
+    } else {
+      // verify subscription
+      const paywallResult: PAYWALL_RESULT =
+        await Paywall.presentPaywallIfNeeded({
+          requiredEntitlementIdentifier: 'Pro',
+        });
+      switch (paywallResult) {
+        case PAYWALL_RESULT.ERROR:
+        case PAYWALL_RESULT.CANCELLED:
+          return false;
+        case PAYWALL_RESULT.NOT_PRESENTED:
+        case PAYWALL_RESULT.PURCHASED:
+        case PAYWALL_RESULT.RESTORED:
+          return router.push(`/meditation/${meditation.id}`);
+        default:
+          return false;
+      }
+    }
+  };
+
   return (
-    <TouchableOpacity activeOpacity={0.7} onPress={() => router.push(`/meditation/${meditation.id}`)} className="p-5 border-2 border-gray-300 rounded-2xl">
+    <TouchableOpacity activeOpacity={0.7} onPress={() => onPress()} className="p-5 border-2 border-gray-300 rounded-2xl">
       <View className="flex-row items-center gap-5">
         <View className="bg-green-700 p-2 rounded-full">
           <FontAwesome name="check" size={16} color="white" />
